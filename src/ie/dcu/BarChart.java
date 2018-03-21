e92cb6bbc2fd5720a44ff37825ed034807161e75
Bernard O'Connor <bernard.oconnor47@mail.dcu.ie>
2018-03-21 20:38:56 +0000

    Show total Cars in park and looking for spaces

--- src/ie/dcu/BarChart.java
+++ src/ie/dcu/BarChart.java
@@ -12,6 +12,8 @@ public class BarChart extends JPanel {
     private int goingIn;
     private int entrance;
     private int spacesAvailable;
+    private int lookingForSpace;
+    private int totalInCarPark;
     private int exit;
     private int gone;
     private int max;
@@ -19,10 +21,12 @@ public class BarChart extends JPanel {
     BarChart(int total) {
         max = total;
         goingIn = total;
-        entrance = spacesAvailable = exit = gone = 0;
+        entrance = totalInCarPark = lookingForSpace= spacesAvailable = exit = gone = 0;
         addBar("Going In", Color.lightGray, total);
         addBar("Entrance", Color.green, 0);
+        addBar("Total In CarPark", Color.blue, 0);
         addBar("Spaces Available",Color.cyan, total);
+        addBar("Looking For Space",Color.orange, 0);
         addBar("Exit",Color.red, 0);
         addBar("Gone",Color.black, 0);
     }
@@ -34,9 +38,13 @@ public class BarChart extends JPanel {
 
     private void updateBar(String label, Color color, int count) {
         Bar bar = bars.get(label);
-        bar.setColor(color);
-        bar.setCount(count);
-        repaint();
+        if(bar == null){
+            addBar(label, color, count);
+        } else {
+            bar.setColor(color);
+            bar.setCount(count);
+            repaint();
+        }
     }
 
     public void update() {
@@ -44,7 +52,9 @@ public class BarChart extends JPanel {
         //bars.clear();
         updateBar("Going In", Color.lightGray, goingIn);
         updateBar("Entrance", Color.green, entrance);
+        updateBar("Total In CarPark", Color.blue, totalInCarPark);
         updateBar("Spaces Available", Color.cyan, spacesAvailable);
+        updateBar("Looking For Space",Color.orange, lookingForSpace);
         updateBar("Exit", Color.red, exit);
         updateBar("Gone", Color.black, gone);
         repaint();
@@ -60,7 +70,7 @@ public class BarChart extends JPanel {
             int height = (int) ((getHeight() - 5) * ((double) value / max));
             g.setColor(bar.getColor());
             g.fillRect(x, getHeight() - height, width, height);
-            g.drawString(bar.getLabel(), x, getHeight()-height);
+            g.drawString(bar.getLabel() + ": " + bar.getCount(), x, getHeight()-height);
             g.setColor(Color.black);
             g.drawRect(x, getHeight() - height, width, height);
             x += (width + 2);
@@ -82,6 +92,14 @@ public class BarChart extends JPanel {
 
     public synchronized void setSpacesAvailable(int spacesAvailable) {
         this.spacesAvailable = spacesAvailable;
+    }
+
+    public synchronized void setLookingForSpace(int lookingForSpace) {
+        this.lookingForSpace = lookingForSpace;
+    }
+
+    public synchronized void setTotalInCarPark(int totalInCarPark) {
+        this.totalInCarPark = totalInCarPark;
     }
 
     public synchronized void setExit(int exit) {
